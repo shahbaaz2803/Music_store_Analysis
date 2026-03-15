724365c644c8d4efa7f6143e282ef011fc8a9f56
Shaik Mohammed Shahbaaz <shahbaazshaik0328@gmail.com>
2026-03-15 22:04:00 +0530

    Create Music_Store_Table_Creation_import.sql

---
+++ SQL_Files/Music_Store_Table_Creation_import.sql
@@ -0,0 +1,149 @@
+CREATE DATABASE musicstore;
+USE musicstore;
+
+-- 1. Genre 
+CREATE TABLE Genre (
+    genre_id INT PRIMARY KEY,
+    name VARCHAR(120)
+);
+
+-- 2. MediaType
+CREATE TABLE MediaType (
+	media_type_id INT PRIMARY KEY,
+	name VARCHAR(120)
+);
+
+-- 2. Employee
+CREATE TABLE Employee (
+	employee_id INT PRIMARY KEY,
+	last_name VARCHAR(120),
+	first_name VARCHAR(120),
+	title VARCHAR(120),
+	reports_to INT,
+  levels VARCHAR(255),
+	birthdate DATE,
+	hire_date DATE,
+	address VARCHAR(255),
+	city VARCHAR(100),
+	state VARCHAR(100),
+	country VARCHAR(100),
+	postal_code VARCHAR(20),
+	phone VARCHAR(50),
+	fax VARCHAR(50),
+	email VARCHAR(100)
+);
+
+
+-- 3. Customer
+CREATE TABLE Customer (
+	customer_id INT PRIMARY KEY,
+	first_name VARCHAR(120),
+	last_name VARCHAR(120),
+	company VARCHAR(120),
+	address VARCHAR(255),
+	city VARCHAR(100),
+	state VARCHAR(100),
+	country VARCHAR(100),
+	postal_code VARCHAR(20),
+	phone VARCHAR(50),
+	fax VARCHAR(50),
+	email VARCHAR(100),
+	support_rep_id INT,
+	FOREIGN KEY (support_rep_id) REFERENCES Employee(employee_id)
+);
+
+-- 4. Artist
+CREATE TABLE Artist (
+	artist_id INT PRIMARY KEY,
+	name VARCHAR(120)
+);
+
+-- 5. Album
+CREATE TABLE Album (
+	album_id INT PRIMARY KEY,
+	title VARCHAR(160),
+	artist_id INT,
+	FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
+);
+
+-- 6. Track
+CREATE TABLE Track (
+	track_id INT PRIMARY KEY,
+	name VARCHAR(200),
+	album_id INT,
+	media_type_id INT,
+	genre_id INT,
+	composer VARCHAR(220),
+	milliseconds INT,
+	bytes INT,
+	unit_price DECIMAL(10,2),
+	FOREIGN KEY (album_id) REFERENCES Album(album_id),
+	FOREIGN KEY (media_type_id) REFERENCES MediaType(media_type_id),
+	FOREIGN KEY (genre_id) REFERENCES Genre(genre_id)
+);
+
+-- 7. Invoice
+CREATE TABLE Invoice (
+	invoice_id INT PRIMARY KEY,
+	customer_id INT,
+	invoice_date DATE,
+	billing_address VARCHAR(255),
+	billing_city VARCHAR(100),
+	billing_state VARCHAR(100),
+	billing_country VARCHAR(100),
+	billing_postal_code VARCHAR(20),
+	total DECIMAL(10,2),
+	FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
+
+-- 8. InvoiceLine
+CREATE TABLE InvoiceLine (
+	invoice_line_id INT PRIMARY KEY,
+	invoice_id INT,
+	track_id INT,
+	unit_price DECIMAL(10,2),
+	quantity INT,
+	FOREIGN KEY (invoice_id) REFERENCES Invoice(invoice_id),
+	FOREIGN KEY (track_id) REFERENCES Track(track_id)
+);
+
+-- 9. Playlist
+CREATE TABLE Playlist (
+ 	playlist_id INT PRIMARY KEY,
+	name VARCHAR(255)
+);
+
+-- 10. PlaylistTrack
+CREATE TABLE PlaylistTrack (
+	playlist_id INT,
+	track_id INT,
+	PRIMARY KEY (playlist_id, track_id),
+	FOREIGN KEY (playlist_id) REFERENCES Playlist(playlist_id),
+	FOREIGN KEY (track_id) REFERENCES Track(track_id)
+);
+
+INSERT INTO employee
+    values 
+    (9,'Madan','Mohan','Senior General Manager',NULL,'L7',
+    '1961-01-26','2002-01-14',
+    '1008 Vrine','Edmonton','AB','Canada','T5K 2N1',
+    '+1 (780) 428-9482',
+    '+1 (780) 428-3457',
+    'madan.mohan@chinookcorp.com');
+
+SELECT * FROM Employee;
+
+--  Checking Tables in DB
+SHOW TABLES;
+
+DESC genre;
+DESC mediatype;
+DESC employee;
+DESC customer;
+DESC artist;
+DESC album;
+DESC track;
+DESC invoice;
+DESC invoiceline;
+DESC playlist;
+DESC playlisttrack;
